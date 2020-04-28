ebcf48c2fb68b3abd6cd90cbfee545b5a7f75aa5
Jesse Lintunen <jesse.lintunen13@gmail.com>
2020-04-28 12:53:00 +0300

    start

---
+++ array.js
@@ -0,0 +1,9 @@
+let car = "Volvo";
+let car2 = "BMW";
+
+let cars = new Array("Volvo", "BMW", "Ford");
+let fruits = ["apples", "oranges", "pears"];
+
+console.log(cars[1]);
+cars[3] = "flat";
+console.log(cars);
